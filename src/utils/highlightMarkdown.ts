3ce69719dcdacaed9499394d0907681d4771c07d
yuseipen0716 <yuseipen1@gmail.com>
2023-04-06 00:03:43 +0900

    codeのシンタックスハイライトが済んだものを変数htmlに渡して出力するように修正

---
+++ src/utils/highlightMarkdown.ts
@@ -0,0 +1,22 @@
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+
+export function highlightMarkdown(html: string) {
+  const $ = cheerio.load(html);
+  // codeブロックの部分のsyntax highlightを行う。
+  // 初回アクセス時にもスタイルが当たっているように。
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+
+  // p code部分にも網掛けのスタイルを当てたい
+  //（当たっているが、サイトの背景色の関係で見えないため。網掛けを濃く）
+  $('p code').each((_, elm) => {
+    $(elm).html();
+    $(elm).addClass('hljs');
+  });
+
+  return $.html();
+}

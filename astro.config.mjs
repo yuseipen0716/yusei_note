b7666a8e13beef5485c3f93f385fff015b4555ea
yuseipen0716 <yuseipen1@gmail.com>
2023-04-07 15:47:26 +0900

    analytics用の設定や計測用のscript追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
